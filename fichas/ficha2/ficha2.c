5e0ee15a87e583f9c7349cb150d14a4423a5bc63
José Vasconcelos <zevasconcelos6@gmail.com>
2022-09-13 12:42:02 +0100

    Alinea 1

--- fichas/ficha2/ficha2.c
+++ fichas/ficha2/ficha2.c
@@ -7,13 +7,16 @@ float multInt1 (int n, float m){
     for(i=1;i!=n;i++){
         r += i*m;
     }
+    printf("r = %d\n",r);
     return r;
 }
 
 
 
 int main(){
-   // int n;
-   // float m;
-    multInt1(n,m);
+  int n;
+  float m;
+  printf("Introduza a variável n e m\n");
+  scanf("%d %f",&n,&m);
+  multInt1(n,m);
 }
