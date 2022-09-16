9bf72d830b593765457a393426958e75fd21a92f
José Vasconcelos <zevasconcelos6@gmail.com>
2022-09-16 16:59:54 +0100

    Menu da ficha2

--- fichas/ficha2/ficha2.c
+++ fichas/ficha2/ficha2.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 
 //1
-float multInt1 (int n, float m){
+float multInt1 (){
+    int n;
+    float m;
+    printf("Introduza as variáveis n e m\n");
+    scanf("%d %f",&n,&m);
     float r = 0;
     int i,e;
     for(i=1;i<=n;i++){
@@ -16,7 +20,11 @@ float multInt1 (int n, float m){
 }
 
 //2
-float multInt2 (int n, float m){
+float multInt2 (){
+   int n;
+   float m;
+   printf("Introduza as variáveis n e m\n");
+   scanf("%d %f",&n,&m);
    float r = 0;
    int e = 0;
    while(n >= 1){
@@ -31,17 +39,18 @@ float multInt2 (int n, float m){
     // printf("m = %f\n",m);
    }
    printf("r = %f\n",r);
-   printf("eficiência: %d\n",e);
+   printf("eficiência: %d\n\n",e);
    return r;
 }
 
 //3
-// int mdc1(int a, int b){
+//int mdc1(){
 
-// }
+ //}
 int main(){
-  int r;
-    printf("\nOlá, seja bem-vindo(a) à ficha2 de PI. Escolha qual exercício deseja realizar.\n");
+  int r=1;
+  do{
+    printf("\nOlá, seja bem-vindo(a) à ficha2 de PI. Escolha qual exercício deseja realizar.\n\n");
     printf("1. Produto de n por m\n");
     printf("2. Produto de n por m (mais eficiente)\n");
     printf("3. Máximo divisor comum entre a e b\n");
@@ -49,52 +58,58 @@ int main(){
     printf("5. Máximo divisor comum entre a e b (Euclides)(%%)\n");
     printf("6. Sequência de Fibonacci\n");
     printf("7. Sair :(\n");
-
+    
     scanf("%d",&r);
-    system("clear");
-    if(r==1){
+    system("cls || clear");
+
+    switch(r) {
+
+      case 1:
+        multInt1();
+        break;
+
+        case 2:
+          multInt2();
+          break;
+        /*
+        case 3:
         int n;
         float m;
-        printf("\nIntroduza as variáveis n e m\n");
+        printf("Introduza as variáveis n e m\n");
         scanf("%d %f",&n,&m);
         multInt1(n,m);
-    }
-    else if(r==2){
+        break;
+
+        case 4:
         int n;
         float m;
-        printf("\nIntroduza as variáveis n e m\n");
+        printf("Introduza as variáveis n e m\n");
         scanf("%d %f",&n,&m);
-        multInt2(n,m);
-        }
-       /*
-       else if(r==3){
-              int a,b;
-              printf("\nIntroduza as variáveis a e b\n");
-              scanf("%d %d",&a,&b);
-              mdc1(a,b);
-            }
-            else if(r==4){
-                  int a,b;
-                  printf("\nIntroduza as variáveis a e b\n");
-                  scanf("%d %d",&a,&b);
-                  mdc1(a,b);
-                 }
-                 else if(r==5){
-                        int a,b;
-                        printf("\nIntroduza as variáveis a e b\n");
-                        scanf("%d %d",&a,&b);
-                        mdc1(a,b);
-                      }
-                      else if(r==6){
-                              int a,b;
-                              printf("\nIntroduza as variáveis a e b\n");
-                              scanf("%d %d",&a,&b);
-                              mdc1(a,b);
-                           }
-                           */
-                           else if(r==7){
-                                  system("exit");
-                                }
-       
- return 1;
+        multInt1(n,m);
+        break;
+
+        case 5:
+        int n;
+        float m;
+        printf("Introduza as variáveis n e m\n");
+        scanf("%d %f",&n,&m);
+        multInt1(n,m);
+        break;
+
+        case 6:
+        int n;
+        float m;
+        printf("Introduza as variáveis n e m\n");
+        scanf("%d %f",&n,&m);
+        multInt1(n,m);
+        break;
+        */
+        case 7:
+          printf("Já vais? :(\n\n");
+          break;
+        
+        default:
+          printf("Valor inválido\n");
+    }
+  } while(r!=7);
 }
