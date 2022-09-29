60beb47e7186aa6e5dc4076b88e426239fc723ef
José Vasconcelos <zevasconcelos6@gmail.com>
2022-09-29 10:29:21 +0100

    Ex1 ficha5

--- fichas/ficha5/ficha5.c
+++ fichas/ficha5/ficha5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 typedef struct  aluno {
     int numero;
@@ -7,15 +8,34 @@ typedef struct  aluno {
     float teste;
 } Aluno;
 
-int nota(){
+float nota(){
     Aluno a;
+    int i;
+    double mini,nota,media1,media2;
     printf("Introduza o número do aluno\n");
-    scanf("%d",&numero);
+    scanf("%d",&a.numero);
     printf("Introduza o nome do aluno\n");
-    scanf("%c",&nome);
+    scanf("%s",&a.nome);
     printf("Introduza as notas dos Mini-testes\n");
-    scanf("%d")
-
+    for(i=0;i<6;i++){
+        scanf("%d",&a.miniT[i]);
+        if(a.miniT[i]<0 || a.miniT[i]>2){
+            printf("Erro! Introduza de novo a sua nota\n");
+            scanf("%d",&a.miniT[i]);
+        }
+    }
+    for(i=0;i<6;i++){
+        mini += a.miniT[i]; 
+    }
+    media1 = (mini/6 + fmod(mini,6.0));
+    printf("Media1 = %f\n",media1);
+    media2 = ((media1*5)/2) + fmod((media1*5),2);
+    printf("Media2 = %f\n",media2);
+    printf("Introduza a nota do teste\n");
+    scanf("%f",&a.teste);
+    nota = 0.75*a.teste + media2;
+    printf("A nota final do aluno é: %f\n",nota);
+    return nota;
 }
 int main(){
      nota();
